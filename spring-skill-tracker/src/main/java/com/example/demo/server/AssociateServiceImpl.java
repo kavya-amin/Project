2c43b37c97f94d1e4f91a36d7126eb923c3356e2
b8ibmjava19@iiht.tech <b8ibmjava19@iiht.tech>
2019-11-11 23:08:51 +0530

    entity added

--- spring-skill-tracker/src/main/java/com/example/demo/server/AssociateServiceImpl.java
+++ spring-skill-tracker/src/main/java/com/example/demo/server/AssociateServiceImpl.java
@@ -1,5 +1,10 @@
 package com.example.demo.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AssociateServiceImpl implements AssociateService {
+	
+	@Autowired
+	private AssociateService associateService;
 
 }
--- spring-skill-tracker/src/main/java/com/example/demo/server/HRServiceImpl.java
+++ spring-skill-tracker/src/main/java/com/example/demo/server/HRServiceImpl.java
@@ -1,5 +1,10 @@
 package com.example.demo.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class HRServiceImpl implements HRService {
+	
+	@Autowired
+	private HRService hrService;
 
 }
